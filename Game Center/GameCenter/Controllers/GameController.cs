8bfd5a8f6ffd09ad8f63e97acd9e6fa855493cf4
Ulrik Rosberg <rojo19ey@student.ju.se>
2021-06-05 17:48:10 +0200

    upload start project

---
+++ Game Center/GameCenter/Controllers/GameController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameCenter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+    }
+}
